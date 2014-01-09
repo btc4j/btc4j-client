f1458a48d5a9d75766dd4fadeda3bf0daffc197a
ggbusto <ggonzalez@holchan.com>
2014-01-09 08:38:54 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-btc4j-client
-============
+Bitcoin Java Client (btc4j-client)
+==================================
+Open source Java implementation of the Bitcoin client.
+
+Using btc4j-client
+------------------
+Snapshot builds are available from GitHub btc4j-repo:
+
+		<dependency>
+			<groupId>org.btc4j</groupId>
+			<artifactId>btc4j-client</artifactId>
+			<version>0.0.1-SNAPSHOT</version>
+		</dependency>
+		
+Add the following to your pom.xml:
+
+		<repositories>
+			<repository>
+				<id>btc4j-repo-snapshots</id>
+				<url>https://github.com/btc4j/btc4j-repo/snapshots</url>
+			</repository>
+		</repositories>
+
+Development build status: [![Build Status](https://travis-ci.org/btc4j/btc4j-client.png?branch=master)](https://travis-ci.org/btc4j/btc4j-client)
+
+btc4j-client is free software under [The MIT License (MIT)](http://opensource.org/licenses/MIT/ "The MIT License (MIT)"). It is maintained by Guillermo Gonzalez (email@ggonzalez.info).
+
+Issues
+------
+Report bugs, issues, or suggestions: https://github.com/btc4j/btc4j-client/issues.
+
+Contributions
+-------------
+Project location: https://github.com/btc4j/btc4j-client.
+* git: `git@github.com:btc4j/btc4j-client.git`
+* svn: `https://github.com/btc4j/btc4j-client`
+
+Donations
+---------
+If you find the software useful and would like to make a donation, please send bitcoins to `1ACz6GKw3B6vjoYwGA2hnXDnbR5koFhC4j`.
+
+Copyright &copy; 2013, 2014 by Guillermo Gonzalez, btc4j.org.
---
+++ src/test/java/org/btc4j/client/BitcoinClientTest.java
@@ -0,0 +1,62 @@
+/*
+ The MIT License (MIT)
+ 
+ Copyright (c) 2013, 2014 by Guillermo Gonzalez, btc4j.org
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+
+package org.btc4j.client;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.btc4j.core.BitcoinException;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class BitcoinClientTest {
+	private static BitcoinClient BITCOINC;
+
+	@BeforeClass
+	public static void testSetup() throws Exception {
+		BITCOINC = new BitcoinClient();
+	}
+
+	@AfterClass
+	public static void testCleanup() throws Exception {
+		String stop = BITCOINC.stop();
+		assertNotNull(stop);
+		assertTrue(stop.length() >= 0);
+	}
+
+	@Test
+	public void help() throws BitcoinException {
+		String help = BITCOINC.help();
+		assertNotNull(help);
+		assertTrue(help.length() >= 0);
+		help = BITCOINC.help("fakecommand");
+		assertNotNull(help);
+		assertTrue(help.length() >= 0);
+		help = BITCOINC.help("getrawtransaction");
+		assertNotNull(help);
+		assertTrue(help.length() >= 0);
+	}
+}
